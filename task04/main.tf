3289f923b5df2ec35a408ddfcd5a94c50adff7ca
javeed939 <50522116+javeed939@users.noreply.github.com>
2025-10-16 17:42:59 +0530

    Add security rules for SSH and HTTP in NSG
    Added security rules for SSH and HTTP to the NSG configuration, removing standalone rules.

--- task04/main.tf
+++ task04/main.tf
@@ -43,40 +43,35 @@ resource "azurerm_network_security_group" "nsg" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
+  security_rule {
+    name                       = var.azure_ssh
+    priority                   = 200
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_ranges    = ["22"]    # list form (required by provider)
+    source_address_prefix      = "0.0.0.0/0"   # consider restricting this
+    destination_address_prefix = "*"
+  }
+
+ security_rule {
+    name                       = var.azure_http
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_ranges    = ["80"]    # list form (required by provider)
+    source_address_prefix      = "0.0.0.0/0"
+    destination_address_prefix = "*"
+  }
+
   tags = {
     Creator = var.creator_tag_name
   }
 }
 
-# Standalone NSG rule for HTTP
-resource "azurerm_network_security_rule" "allow_http" {
-  name                        = var.azure_http
-  priority                    = 100
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "Tcp"
-  source_port_range           = "*"
-  destination_port_ranges     = ["80"]
-  source_address_prefix       = "0.0.0.0/0"
-  destination_address_prefix  = "*"
-  resource_group_name         = azurerm_resource_group.rg.name
-  network_security_group_name = azurerm_network_security_group.nsg.name
-}
-
-# Standalone NSG rule for SSH
-resource "azurerm_network_security_rule" "allow_ssh" {
-  name                        = var.azure_ssh
-  priority                    = 200
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "Tcp"
-  source_port_range           = "*"
-  destination_port_ranges     = ["22"]
-  source_address_prefix       = "0.0.0.0/0"
-  destination_address_prefix  = "*"
-  resource_group_name         = azurerm_resource_group.rg.name
-  network_security_group_name = azurerm_network_security_group.nsg.name
-}
 
 resource "azurerm_network_interface" "nic" {
   name                = var.azure_nic

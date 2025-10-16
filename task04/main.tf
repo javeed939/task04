301c5c33d6bacd16ecb39f0847eb675e530be8ec
javeed939 <50522116+javeed939@users.noreply.github.com>
2025-10-16 17:39:13 +0530

    Update main.tf

--- task04/main.tf
+++ task04/main.tf
@@ -56,7 +56,7 @@ resource "azurerm_network_security_rule" "allow_http" {
   access                      = "Allow"
   protocol                    = "Tcp"
   source_port_range           = "*"
-  destination_port_ranges     = "80"
+  destination_port_ranges     = ["80"]
   source_address_prefix       = "0.0.0.0/0"
   destination_address_prefix  = "*"
   resource_group_name         = azurerm_resource_group.rg.name
@@ -71,7 +71,7 @@ resource "azurerm_network_security_rule" "allow_ssh" {
   access                      = "Allow"
   protocol                    = "Tcp"
   source_port_range           = "*"
-  destination_port_ranges     = "22"
+  destination_port_ranges     = ["22"]
   source_address_prefix       = "0.0.0.0/0"
   destination_address_prefix  = "*"
   resource_group_name         = azurerm_resource_group.rg.name
